20d735e7ec9b843b854ebfebd50f94ed8a0d6670
Oliver Ryall <oliryall0404@gmail.com>
2022-12-09 18:12:34 +0100

    add: opgoerli network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    // --------------- TESTNETS ---------------
+    opgoerli: {
+      url: process.env.OPTIMISM_GOERLI_URL || "",
+      chainId: 420,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
 
 export default config;
